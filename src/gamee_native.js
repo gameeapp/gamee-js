19ae168c9b26b48ffb808bc1175af828b9d244b1
Peter Morihladko <morihladko@gmail.com>
2015-03-03 15:03:14 +0100

    Gamee web support

--- src/gamee_native.js
+++ src/gamee_native.js
@@ -1,11 +1,10 @@
 /**
- * Shim for $gameeNative object
+ * Wrappers for $gameeNative object in diferent envitorments
  */
 (function(global) {
 	'use strict';
 	
-	var simulator,
-		gameeNative = {
+	var gameeNative = {
 			/**
 			 * Update score 
 			 *
@@ -29,7 +28,75 @@
 			 * Game start
 			 */ 
 			gameStart: function() {}
-		},
+		};
+		
+		/**
+		 * Gamee Mobile App 
+		 */
+		function gameeMobile(gameeNative) {
+			gameeNative.updateScore = function(score) {
+				window.location.href = "gamee://score/" + score;
+			};
+
+			gameeNative.requestController = function(type) {
+				window.location.href = "gamee://request-controller/" + type;
+			};
+
+			gameeNative.gameOver = function() {
+				window.location.href = "gamee://game-over";
+			};
+
+			gameeNative.gameStart = function() {
+				window.location.href = "gamee://game-start";
+			};
+		}
+
+		/**
+		 * Gamee simulator and validator
+		 */
+		function gameeSimulator(gameeNative) {
+			var simulator = window.parent.gameeSimulator;
+
+			gameeNative.updateScore = function(score) {
+				simulator.updateScore(score);
+			};
+
+			gameeNative.requestController = function(type) {
+				simulator.requestController(type);
+			};
+
+			gameeNative.gameOver = function() {
+				simulator.gameOver();
+			};
+
+			gameeNative.gameStart = function() {
+				simulator.gameStart();
+			};
+		}
+
+		/**
+		 * Gamee desktop web view
+		 */
+		function gameeWeb(gameeNative) {
+			var gameeUI = window.parent.gameeUI;
+
+			gameeNative.updateScore = function(score) {
+				gameeUI.updateScore(score);
+			};
+
+			gameeNative.requestController = function(type) {
+				// TODO dohodnut podporu s vyvojarom
+				// gameeUI.requestController(type);
+			};
+
+			gameeNative.gameOver = function() {
+				gameeUI.gameOver();
+			};
+
+			gameeNative.gameStart = function() {
+				gameeUI.gameStart();
+			};
+		}
 
 		userAgent = navigator.userAgent.toLowerCase();
 
@@ -37,41 +104,15 @@
 		/gamee\/[0-9\.]+$/.test(userAgent) || // test for android webview
 		/iphone|ipod|ipad/.test(userAgent)    // test for iOS webview
 	) {
-		gameeNative.updateScore = function(score) {
-			window.location.href = "gamee://score/" + score;
-		};
+		gameeMobile(gameeNative);
 
-		gameeNative.requestController = function(type) {
-			window.location.href = "gamee://request-controller/" + type;
-		};
+	} else if (window.parent && window.parent.gameeUI) {
+		gameeWeb(gameeNative);
 
-		gameeNative.gameOver = function() {
-			window.location.href = "gamee://game-over";
-		};
-
-		gameeNative.gameStart = function() {
-			window.location.href = "gamee://game-start";
-		};
-	} else if (
-		window.parent && window.parent.gameeSimulator
-	) {
-		simulator = window.parent.gameeSimulator;
-
-		gameeNative.updateScore = function(score) {
-			simulator.updateScore(score);
-		};
-
-		gameeNative.requestController = function(type) {
-			simulator.requestController(type);
-		};
-
-		gameeNative.gameOver = function() {
-			simulator.gameOver();
-		};
-
-		gameeNative.gameStart = function() {
-			simulator.gameStart();
-		};
+	} else if (window.parent && window.parent.gameeSimulator) {
+		gameeSimulator(gameeNative);
+	} else {
+		console.error('No gamee enviroment');
 	}
 
 	global.$gameeNative = gameeNative;
